7ef0a890faad6fcb331130b74c5039084b82dc92
ManuLosta <mlostalom@gmail.com>
2024-06-20 18:21:15 -0300

    login done

--- src/app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="grid grid-cols-5">
-			<div className="col-span-2 bg-slate-900 min-h-screen"></div>
-			{children}
-		</div>
-	)
-} 
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,82 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+const FormSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(4)
+})
 
 export default function Login() {
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+		defaultValues: {
+			email: "",
+			password: ""
+		},
+	})
+
+	function onSubmit(data: z.infer<typeof FormSchema>) {
+		console.log(data);
+	}
+
 	return (
-		<div className="flex col-span-3 min-h-screen items-center justify-center flex-col gap-2">
-			<h1 className="font-bold text-lg">Iniciar Sesión</h1>
-			<form className="flex flex-col gap-3">
-				<Input />
-				<Input />
-				<Button>Iniciar Sesión</Button>
-			</form>
+		<div className="w-full h-screen flex items-center justify-center px-4 ">
+			<Card className="w-full max-w-sm">
+				<CardHeader>
+					<CardTitle>Iniciar Sesión</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input type="email" {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Contraseña</FormLabel>
+										<FormControl>
+											<Input type="password" {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<Button className="w-full" type="submit">Iniciar Sesión</Button>
+						</form>
+					</Form>
+
+				</CardContent>
+			</Card>
+
 		</div>
 	)
 }
+
