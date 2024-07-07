d350002c67e8ca2b69c5284e0a512461be727d66
ManuLosta <mlostalom@gmail.com>
2024-07-07 19:43:54 -0300

    fix toaster breaking hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,23 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PageWrapper from "@/components/page-wrapper";
-import {Toaster} from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "CarShare",
+  title: "CarShare",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<PageWrapper>
-				<body className={inter.className}>
-				{children}
-				<Toaster />
-				</body>
-			</PageWrapper>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <PageWrapper>
+        <body className={inter.className}>{children}</body>
+      </PageWrapper>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import TripList from "@/components/trip-list";
 import Stats from "@/components/stats";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function Page() {
-	return (
-		<main className="container mx-auto flex gap-5 mt-10 flex-col">
-			<Stats />
-			<TripList />
-		</main>
-	)
+  return (
+    <main className="container mx-auto flex gap-5 mt-10 flex-col">
+      <Stats />
+      <TripList />
+      <Toaster />
+    </main>
+  );
 }
